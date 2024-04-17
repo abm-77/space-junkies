e31a1787021cea824fcea529a9fe27d2da2646cc
Noah Morales <nmorale5@mit.edu>
2024-04-17 15:05:37 -0400

    working player throw script

--- Assets/Scripts/InertialObject.cs
+++ Assets/Scripts/InertialObject.cs
@@ -6,9 +6,6 @@ using R3;
 
 public class InertialObject : MonoBehaviour
 {
-    [SerializeField] private Vector2 _velocity = Vector2.zero;
-    [SerializeField] private float _mass = 1;
-
     public readonly ReactiveProperty<Vector2> Velocity = new(Vector2.zero);
     public readonly ReactiveProperty<float> Mass = new(1);
 
@@ -19,22 +16,12 @@ public class InertialObject : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
         Velocity.Subscribe(newVel => _rb.velocity = newVel).AddTo(this);
         Mass.Subscribe(newMass => _rb.mass = newMass).AddTo(this);
-        Velocity.Value = _velocity;
-        Mass.Value = _mass;
+        Velocity.Value = _rb.velocity;
+        Mass.Value = _rb.mass;
     }
 
     private void LateUpdate()
     {
         Velocity.Value = _rb.velocity;
-    }
-
-    public void ApplyImpulse(Vector2 impulse)
-    {
-        Velocity.Value += impulse / Mass.Value;
-    }
-
-    public static Vector2 GetImpulse(Vector2 velocity, float mass)
-    {
-        return velocity * mass;
     }
 }
--- Assets/Scripts/ThrowProjectile.cs
+++ Assets/Scripts/ThrowProjectile.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
 
-public class ThrowProjectile : MonoBehaviour, IPointerExitHandler
+public class ThrowProjectile : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private InertialObject _player;
     [SerializeField] private InertialObject _projectilePrefab;
     [SerializeField] private float _throwImpulse = 100;
 
-    public InertialObject SelectedProjectile => _projectilePrefab; // TODO: later, you should be able to change your selected projectile via an inventory
+    public InertialObject SelectedProjectile =>
+        _projectilePrefab; // TODO: later, you should be able to change your selected projectile via an inventory
+    
 
-    public void OnPointerExit(PointerEventData pointerData)
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+    }
+
+    public void OnPointerUp(PointerEventData pointerData)
     {
         var clickPosition3D = pointerData.pointerCurrentRaycast.worldPosition;
         var clickPosition = new Vector2(clickPosition3D.x, clickPosition3D.y);
         var playerPosition3D = _player.transform.position;
         var playerPosition = new Vector2(playerPosition3D.x, playerPosition3D.y);
         var throwDirection = (clickPosition - playerPosition).normalized;
-        var throwVelocity = _throwImpulse * throwDirection / SelectedProjectile.Velocity.Value;
-        var thrownProjectile = Instantiate(_projectilePrefab, transform.position, Quaternion.identity);
+        var throwVelocity = _throwImpulse * throwDirection / SelectedProjectile.Mass.Value;
+        var thrownProjectile = Instantiate(SelectedProjectile, playerPosition3D, Quaternion.identity);
+        Physics2D.IgnoreCollision(_player.GetComponent<Collider2D>(), thrownProjectile.GetComponent<Collider2D>());
         thrownProjectile.Velocity.Value = throwVelocity;
         _player.Velocity.Value -= throwVelocity * thrownProjectile.Mass.Value / _player.Mass.Value;
     }
