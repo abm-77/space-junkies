f7937b2933ee182e5515987f46d40c86af9fb2c2
Bryson Miller <andrewmiller77@protonmail.com>
2024-04-17 23:30:06 -0400

    Demo changes
    - Added level with sprite assets
    - Removed InertialObject in favor of just using Rigidbody2D component directly
    - Optimized ThrowProjectile to use Input system and remove per frame divisions

--- Assets/Scripts/ThrowProjectile.cs
+++ Assets/Scripts/ThrowProjectile.cs
@@ -1,35 +1,66 @@
-﻿using System;
-using R3;
-using R3.Triggers;
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 
-public class ThrowProjectile : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class ThrowProjectile : MonoBehaviour
 {
-    [SerializeField] private InertialObject _player;
-    [SerializeField] private InertialObject _projectilePrefab;
-    [SerializeField] private float _throwImpulse = 100;
+    [SerializeField]
+    private Rigidbody2D projectile_prefab;
 
-    public InertialObject SelectedProjectile =>
-        _projectilePrefab; // TODO: later, you should be able to change your selected projectile via an inventory
-    
+    [SerializeField]
+    [Range(0.1f, 2.0f)]
+    private float throw_impulse_scalar = 100;
+    [SerializeField]
+    private float inv_max_throw_mag, max_throw_mag = 100.0f;
 
-    public void OnPointerDown(PointerEventData pointerEventData)
+    public Rigidbody2D selected_projectile => projectile_prefab; // TODO: later, you should be able to change your selected projectile via an inventory
+    private Rigidbody2D player;
+    private LineRenderer line;
+    private Vector3[] points = new Vector3[2];
+    private float projectile_to_player_mass_ratio, inv_projectile_mass;
+
+    public void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+
+        line = gameObject.AddComponent<LineRenderer>();
+        line.material = new Material(Shader.Find("Sprites/Default"));
+        line.widthMultiplier = 1.5f;
+        line.positionCount = 2;
+        line.enabled = false;
+
+        inv_max_throw_mag = 1.0f / max_throw_mag;
+        inv_projectile_mass = 1.0f / projectile_prefab.mass;
+        projectile_to_player_mass_ratio = projectile_prefab.mass / player.mass;
     }
 
-    public void OnPointerUp(PointerEventData pointerData)
+    public void Update()
     {
-        var clickPosition3D = pointerData.pointerCurrentRaycast.worldPosition;
-        var clickPosition = new Vector2(clickPosition3D.x, clickPosition3D.y);
-        var playerPosition3D = _player.transform.position;
-        var playerPosition = new Vector2(playerPosition3D.x, playerPosition3D.y);
-        var throwDirection = (clickPosition - playerPosition).normalized;
-        var throwVelocity = _throwImpulse * throwDirection / SelectedProjectile.Mass.Value;
-        var thrownProjectile = Instantiate(SelectedProjectile, playerPosition3D, Quaternion.identity);
-        Physics2D.IgnoreCollision(_player.GetComponent<Collider2D>(), thrownProjectile.GetComponent<Collider2D>());
-        thrownProjectile.Velocity.Value = throwVelocity;
-        _player.Velocity.Value -= throwVelocity * thrownProjectile.Mass.Value / _player.Mass.Value;
+        Vector3 player_pos = player.transform.position;
+        Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse_pos.z = 0;
+
+        Vector3 throw_dir = (mouse_pos - player_pos);
+        float throw_mag = Mathf.Clamp(throw_dir.magnitude, 0, max_throw_mag);
+        Vector3 throw_vector = (throw_dir.normalized * throw_mag);
+
+        if (Input.GetMouseButtonDown(0))
+            line.enabled = true;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            line.enabled = false;
+
+            Rigidbody2D projectile = Instantiate(selected_projectile, player_pos, Quaternion.identity);
+            Vector2 throw_velocity = throw_impulse_scalar * throw_vector * inv_projectile_mass;
+            Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), projectile.GetComponent<Collider2D>());
+            projectile.velocity = throw_velocity;
+            player.velocity -= throw_velocity * projectile_to_player_mass_ratio;
+        }
+
+        points[0] = player_pos;
+        points[1] = player_pos + throw_vector;
+        line.SetPositions(points);
+
+        throw_mag *= inv_max_throw_mag;
+        line.startColor = line.endColor = new Color(throw_mag, 1.0f - throw_mag, 0.0f);
     }
 }
