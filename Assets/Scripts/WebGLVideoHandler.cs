4cf91e1b22df3d227334ae919b9fac3548ff1ffc
Noah Morales <nmorale5@mit.edu>
2024-05-15 01:13:32 -0400

    webgl videos work

---
+++ Assets/Scripts/WebGLVideoHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class WebGLVideoHandler : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<VideoPlayer>().url = System.IO.Path.Combine(Application.streamingAssetsPath, $"{SceneManager.GetActiveScene().name}.mp4");
+    }
+}
